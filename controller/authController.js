513663dc7ccd81d918ae5aa5cd960aa80abc9035
shashank shrivastava <internstudent02@gmail.com>
2025-04-08 09:38:14 +0530

    sign_up

---
+++ controller/authController.js
@@ -0,0 +1,15 @@
+const { default: User } = require("../models/User");
+
+exports.authCobntroller = async (req, res) => {
+  try {
+    const isUser = await User.findOne({ email: req.body.email });
+    if (isUser) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
